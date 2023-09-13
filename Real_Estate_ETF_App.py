22340dc39ca88ae4a6f8da062045d35bde82d4b2
incogamir <122631183+incogamir@users.noreply.github.com>
2023-09-12 19:06:16 -0500

    Added Sidebar to the Real Estate App

--- Real_Estate_ETF_App.py
+++ Real_Estate_ETF_App.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Load your CSV data
 icf_returns_df = pd.read_csv("ICF_Cumulative_Df.csv")
-icf_aglo_evaluation_df = pd.read_csv("Algo_Evaluation_Df.csv")
+icf_aglo_evaluation_df = pd.read_csv("ICF_Algo_Evaluation_Df.csv")
 
 # Define ETFs and their details
 etf_database = {
@@ -52,6 +52,55 @@ def main():
         st.write(f"Financial Analysis for {etf_details['Name']}")
         st.table(icf_aglo_evaluation_df)
         st.line_chart(icf_returns_df)
-
-if __name__ == "__main__":
+    
+    # Streamlit application headings
+    st.markdown("# RealEstateETFApp")
+    st.markdown("## Invest In Real Estate ETFs")
+    st.text(" \n")
+    
+    # Streamlit Sidebar Code - Start
+    st.sidebar.markdown("## ETF Account Addresses & Ethernet Balance in Ether")
+    
+    # Call the 'generate_account' function and save it as the variable 'account'
+    account = generate_account()
+    
+    # Write the client's Ethereuma ccount address to the sidebar
+    st.sidebar.write(account.address)
+    
+    # Call the 'get_balance' function and pass it your account address
+    # Write the returned ether balance to the sidebar
+    balance = get_balance(w3, account.address)
+    st.sidebar.write(balance)
+    
+    #Create a select box to chose a Real Estate ETF
+    etf = st.sidebar.number_input("Select a Real Estate ETF", etfs)
+    
+    # Create a input field to record the number of shares requested
+    shares = st.sidebar.number_input("Number of Shares")
+    
+    st.sidebar.markdown("## Real Estate ETF Name, Price per Share, & Ethereum Address")
+    
+    # Identify the Real Estate ETF
+    etfname = etf_database[etf][0]
+    
+    # Write the RealEstateETF etf's name to the sidebar
+    st.sidebar.write(etfname)
+    
+    # Identify the RealEstateETf etf's closing price
+    closing_price = etf_database[etf][3]
+    
+    # Write the closing price to the sidebar
+    st.sidebar.write(closing_price)
+    
+    # Identify the RealEstateETfs etf's Ethereum Address
+    etfs_address = etf_database[etf][1]
+    
+    # Write the etf's Ethereum Address to the sidebar
+    st.sidebar.write(etfs_address)
+    
+    # Write the total shares in Ether
+    st.sidebar.markdown("## Total Shares in Ether")
+    
+    
+if __name__ = "__main__":
     main()
